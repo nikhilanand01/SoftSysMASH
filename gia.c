3a60dd8d181292a7c3314cc1ceb5b80dd6cd9730
gdouglass1 <59745338+gdouglass1@users.noreply.github.com>
2020-03-03 08:29:23 -0500

    Create gia.c

---
+++ gia.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  printf("Hello World\n");
+char *topicList [] = {"car","number of kids", "future location", "job", "spouse's name", "number of friends", "pet", "amount of money", "honeymoon destination"};
+char *topicOne [] = {"mansion","apartment","shack","house"};
+char *topicTwo;
+char *topicThree;
+char *topicFour;
+
+}
