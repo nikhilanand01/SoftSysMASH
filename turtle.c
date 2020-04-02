413290152fc3cc54de5932e53733dbf9adee139b
Nikhil Anand <nikhil@fudinc.com>
2020-04-02 17:54:49 -0400

    adding turtle

---
+++ turtle.c
@@ -0,0 +1,15 @@
+#include "turtle.h"
+
+int main()
+{
+    turtle_init(300, 300);          // initialize the image to be 600x600
+
+    turtle_forward(50);
+    turtle_turn_left(90);
+    turtle_forward(50);
+    turtle_draw_turtle();
+
+    turtle_save_bmp("output.bmp");  // save the turtle drawing
+
+    return EXIT_SUCCESS;
+}
